062712fe29ce2d86a678fb2c824eef62641267e5
Javier Lara <javier.lara@trocafone.com>
2016-11-09 21:27:19 -0300

    Adding widget for calls

---
+++ ApresMovil/app/src/main/java/com/apres/apresmovil/Emergencias.java
@@ -0,0 +1,50 @@
+package com.apres.apresmovil;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class Emergencias extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:" + "48321665"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, callIntent, 0);
+
+        // Get the layout for the App Widget and attach an on-click listener to the button
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.emergencias);
+        views.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+        views.setOnClickPendingIntent(R.id.actionButton2, pendingIntent);
+
+        // Tell the AppWidgetManager to perform an update on the current App Widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
